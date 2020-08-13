b6f66bd3d224edc8bdaf5ccf1ac7a44808b104e5
Andre Padeti <andrepadeti@hotmail.com>
2020-08-13 17:33:21 -0300

    thirteen_july

--- js/dragging.js
+++ js/dragging.js
@@ -1,64 +1,32 @@
-/*
-Classes and functions to get the sentences prepared to be used
-*/
-
-class Sentence {
-    constructor(sentence) {
-        this.text = sentence;
-        this.sentenceInOrder = this.text.split(" "); // array that stores the split sentence
-        this.currentCorrectWord = 0; // stores how many words are correct already while student is sorting
-        this.currentlyLeft = []; // array that stores the words still left to be sorted
-        this.complete = false; // becames true when sentence is sorted
-        this.sentenceShuffled = this.shuffle; // array that stores the shuffled sentence
-
-        // this.shuffleWords = this.shuffleWords.bind(this);
-    }
-
-    shuffle() {
-        let auxArray = [...this.sentenceInOrder];
-        // make sure the first word in the shuffled sentence is different from 
-        // the first word in the sentence in order (the while conditional statement)
-        do {
-            this.shuffleWords(auxArray);
-        } while ((auxArray[0].localeCompare(this.sentenceInOrder[0])) == 0);
-        return [...auxArray];
-    }
-
-
-    // auxiliary method that implements Fisher-Yates shuffle
-    shuffleWords(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            let temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        }
-    }
-}
-
-// function to organise all sentence objects in an array
-function loadSentences(sentences) {
-    let result = [];
-    sentences.forEach((currentValue) => {
-        result.push(new Sentence(currentValue));
-    });
-    return result;
-}
-
 $(document).ready( function() {
 
     // future implementation of query string parameters
     // https://www.sitepoint.com/get-url-parameters-with-javascript/
-    const queryString = window.location.search
+    const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const exercise = urlParams.get("ex");
     // console.log(exercise);
-    
-    var sentences = loadSentences(["The first sentence.","The second sentence.","Yet another sentence."]);
+
+    // real sentences. Too long for testing.
+    // var sentences = loadSentences([
+    //   { code: "git add <filename>", words: "Add files to the INDEX." },
+    //   { code: 'git commit -m "commit message"', words: "Commit the changes to the HEAD." },
+    //   { code: "git push origin master", words: "Send the changes to the remote repository." },
+    //   { code: "git branch feature_x", words: 'Create a new branch named "feature_x".'},
+    //   { code: "git merge feature_x", words: 'Merge branch named "feature_x" into your active branch.' },
+    // ]);
+
+    // simpler sentences for testing only
+    var sentences = loadSentences([
+      { code: "The first sentence.", words: "The first sentence." },
+      { code: "The second sentence.", words: "The second sentence." },
+      // { code: "git push origin master", words: "Yet another sentence." },
+    ]);
 
     // the audio files
-    var moveAudio = new Audio("./sounds/move.wav");
-    var sentenceCompleteAudio = new Audio("./sounds/bell.wav");
+    // the bell audio object is instantiated each time a sentence is completed, in case
+    // the student is too fast and completes the next sentence before the audio finishes playing
+    const moveAudio = new Audio("./sounds/move.wav");
     
     // Handlebars templates
     var sentenceSource = $("#sentenceTemplate").html(); // retrieve the template data from the html
@@ -82,6 +50,7 @@ $(document).ready( function() {
             sentences[whichSentence].currentlyLeft = this.toArray();
             var text = $("#"+sentences[whichSentence].currentlyLeft[0]).text();
             
+            // keep marking words sorted until the next one is out of order.
             while ((sentences[whichSentence].sentenceInOrder[sentences[whichSentence].currentCorrectWord].localeCompare(text)) == 0) {
                 $("#"+sentences[whichSentence].currentlyLeft[0]).removeClass("sortable").addClass("notSortable");
                 sentences[whichSentence].currentCorrectWord++;
@@ -91,7 +60,7 @@ $(document).ready( function() {
                 if ( sentences[whichSentence].currentlyLeft.length == 0 ) { 
                     sentences[whichSentence].complete = true;
                     $("#icon"+whichSentence).attr("src", "./images/tick.png");
-                    var sentenceCompleteAudio = new Audio("./sounds/bell.wav");
+                    const sentenceCompleteAudio = new Audio("./sounds/bell.wav");
                     sentenceCompleteAudio.play();
                     break; 
                 }
@@ -104,15 +73,11 @@ $(document).ready( function() {
                 if (!sorted) break;
             }
             // if all sorted, show modal
-            if (sorted) {
-                $("#completeModal").modal("show");
-            }
+            if (sorted) $("#completeModal").modal("show");
         }
     });
 }); 
 
 // handlebars helper to show @index starting from 1
-Handlebars.registerHelper('incremented', function (index) {
-    return ++index;
-});
+Handlebars.registerHelper('incremented', index => ++index);
 
